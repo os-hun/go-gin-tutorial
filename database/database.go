e24a8d1ea0993f75b0510501478da9df1659ca8e
Shun <shun12baseball@gmail.com>
2020-08-11 11:01:49 +0900

    new: modelディレクトリを作成 new: Todo modelを作成 new: auto migrate をdb initに追加

--- database/database.go
+++ database/database.go
@@ -2,13 +2,20 @@ package database
 
 import (
 	"github.com/jinzhu/gorm"
+	"go-gin-tutorial/model"
 	_"github.com/mattn/go-sqlite3"
 )
 
 func DbInit() {
+	// open db
 	db, err := gorm.Open("sqlite3", "test.splite3")
 	if err != nil {
 		panic("can't open the database")
 	}
+	
+	// auto migration
+	db.AutoMigrate(&model.Todo{})
+	
+	// close db
 	defer db.Close()
 }
