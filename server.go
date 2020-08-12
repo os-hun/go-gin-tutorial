07e044380a8c07566a6a7e8213f588be37a622fc
Shun <shun12baseball@gmail.com>
2020-08-13 08:18:39 +0900

    new: server.goにルートを追加

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"github.com/gin-gonic/gin"
 	"go-gin-tutorial/database"
 )
@@ -13,10 +14,68 @@ func main() {
 
 	// Index
 	r.GET("/", func(c *gin.Context) {
-		
+		todos := database.DbGetAll()
 		c.HTML(200, "index.html", gin.H{
 			"todos": todos,
 		})
+	})
+
+	// Create
+	r.POST("/new", func(c *gin.Context) {
+		text := c.PostForm("text")
+		status := c.PostForm("status")
+		database.DbInsert(text, status)
+		c.Redirect(302, "/")
+	})
+
+	// Detail
+	r.GET("/detail/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := database.DbGetOne(id)
+		c.HTML(200, "detail.html", gin.H{
+			"todo": todo,
+		})
+	})
+
+	// Update
+	r.POST("/update/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := c.PostForm("text")
+		status := c.PostForm("status")
+		database.DbUpdate(id, text, status)
+		c.Redirect(302, "/")
+	})
+
+	// 削除確認
+	r.GET("/delete/:id/check", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := database.DbGetOne(id)
+		c.HTML(200, "delete.html", gin.H{
+			"todo": todo,
+		})
+	})
+
+	// Delete
+	r.POST("/delete/:id", func(c *gin.Context) {
+		n := c.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		database.DbDelete(id)
+		c.Redirect(302, "/")
 	})
 
 	// listen and serve on :8080
